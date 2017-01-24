32ef67b22ce0bda729ce7b9970f72e9d2cdbed92
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-01-24 18:22:27 +0700

    handle back stack of event detail

--- ChulaExpo/app/src/main/java/cuexpo/chulaexpo/activity/EventDetailActivity.java
+++ ChulaExpo/app/src/main/java/cuexpo/chulaexpo/activity/EventDetailActivity.java
@@ -1,13 +1,14 @@
-package cuexpo.chulaexpo.fragment;
+package cuexpo.chulaexpo.activity;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,50 +19,48 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 import cuexpo.chulaexpo.R;
 import cuexpo.chulaexpo.adapter.EventDetailListAdapter;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
+public class EventDetailActivity extends AppCompatActivity {
 
-public class EventDetailFragment extends Fragment {
-
-    private View rootView;
     private ListView listView;
     private View eventImageView;
     private FrameLayout headerView;
     private View stickyViewSpacer;
     private View listHeader;
-    private Fragment fragment;
+    private Activity activity;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_event_detail, container, false);
-        fragment = this;
-        listView = (ListView) rootView.findViewById(R.id.list_view);
-        eventImageView = rootView.findViewById(R.id.event_image);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+        activity = this;
+
+        listView = (ListView) findViewById(R.id.list_view);
+        eventImageView = findViewById(R.id.event_image);
         Glide.with(this)
                 .load("http://www.womie.ru/wp-content/uploads/2014/04/%D0%96%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D1%8B-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B5%D1%82%D0%B0%D1%82%D0%B5%D0%BB%D0%B8-1.jpg")
                 .placeholder(R.color.blackOverlay)
                 .centerCrop()
                 .into((ImageView) eventImageView);
-        headerView = (FrameLayout) rootView.findViewById(R.id.header);
-        TextView title = (TextView) rootView.findViewById(R.id.title);
+        headerView = (FrameLayout) findViewById(R.id.header);
+        TextView title = (TextView) findViewById(R.id.title);
         title.setText("การแสดงสาธิต หุ่นยนต์ดูดฝุ่น");
 
-        ImageView closeButton = (ImageView) rootView.findViewById(R.id.close_button);
+        ImageView closeButton = (ImageView) findViewById(R.id.close_button);
         closeButton.setOnClickListener(closeButtonOnClickListener);
 
+        LayoutInflater inflater = getLayoutInflater();
         listHeader = inflater.inflate(R.layout.item_event_detail_header, null);
         stickyViewSpacer = listHeader.findViewById(R.id.sticky_view_placeholder);
 
         ViewTreeObserver vto = headerView.getViewTreeObserver();
         vto.addOnGlobalLayoutListener(onGlobalLayoutListener);
-
-        return rootView;
     }
 
     private AbsListView.OnScrollListener onScrollListener = new AbsListView.OnScrollListener() {
@@ -97,7 +96,7 @@ public class EventDetailFragment extends Fragment {
             listView.addHeaderView(listHeader);
             listView.setOnScrollListener(onScrollListener);
 
-            EventDetailListAdapter adapter = new EventDetailListAdapter(getActivity(), 0);
+            EventDetailListAdapter adapter = new EventDetailListAdapter(activity, 0);
             listView.setAdapter(adapter);
 
             headerView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
@@ -112,8 +111,13 @@ public class EventDetailFragment extends Fragment {
     private View.OnClickListener closeButtonOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            Intent intent = new Intent(activity, MainActivity.class);
+            startActivity(intent);
         }
     };
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 }
