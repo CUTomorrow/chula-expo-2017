0cfdc5aff1827364d82f6cfb99d37932a5c4a21e
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-02-15 14:23:58 +0700

    merge Boom-sama conflict

---
+++ ChulaExpo/app/src/main/java/cuexpo/chulaexpo/adapter/FaqListAdapter.java
@@ -0,0 +1,66 @@
+package cuexpo.chulaexpo.adapter;
+
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
+
+import cuexpo.chulaexpo.dao.ActivityItemCollectionDao;
+import cuexpo.chulaexpo.datatype.MutableInteger;
+import cuexpo.chulaexpo.view.ActivityListItem;
+import cuexpo.chulaexpo.view.FaqListItem;
+
+/**
+ * Created by dragonnight on 26/12/2559.
+ */
+
+public class FaqListAdapter extends BaseAdapter{
+
+    @Override
+    public int getCount() {
+        return 10;
+        /*
+        if(cuexpo.chulaexpo.dao == null) return  0;
+        if(cuexpo.chulaexpo.dao.getData() == null) return 0;
+        return cuexpo.chulaexpo.dao.getData().size();
+        */
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return null;
+        // return cuexpo.chulaexpo.dao.getData().get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        FaqListItem item;
+
+        if(convertView!=null)
+            item = (FaqListItem) convertView;
+        else
+            item = new FaqListItem(parent.getContext());
+
+        //Mock
+        if(position%3==0){
+            item.setQuestion("Vidva Highlight");
+            item.setAnswer("15 Mar 09.40 - 10.40");
+        }
+        else if(position%3==1){
+            item.setQuestion("The Accountant นักบัญชีในตำนาน");
+            item.setAnswer("15 Mar 11.40 - 12.40");
+        } else {
+            item.setQuestion("Psyxcho Highlight");
+            item.setAnswer("15 Mar 13.00 - 15.00");
+        }
+        return item;
+    }
+}
--- ChulaExpo/app/src/main/java/cuexpo/chulaexpo/fragment/ProfileFragment.java
+++ ChulaExpo/app/src/main/java/cuexpo/chulaexpo/fragment/ProfileFragment.java
@@ -1,12 +1,17 @@
 package cuexpo.chulaexpo.fragment;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -24,16 +29,18 @@ import cuexpo.chulaexpo.activity.ReservedActivity;
 @SuppressWarnings("unused")
 public class ProfileFragment extends Fragment implements View.OnClickListener {
 
+    Button btnEdit;
     LinearLayout btnFavourite;
     LinearLayout btnReserved;
-    LinearLayout btnEdit;
-    Button btnSetting;
-    Button btnFb;
-    Button btnAbout;
+    LinearLayout btnSetting;
+    LinearLayout btnSetting2;
+    LinearLayout btnFaq;
+    LinearLayout btnAbout;
+    //Button btnFb;
     Button btnLogout;
     TextView tvName;
     TextView tvSurname;
-    TextView tvType;
+    TextView tvEmail;
     TextView tvYear;
     TextView tvSchool;
     TextView tvAge;
@@ -69,7 +76,9 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         btnReserved.setOnClickListener(this);
         btnEdit.setOnClickListener(this);
         btnSetting.setOnClickListener(this);
-        btnFb.setOnClickListener(this);
+        btnSetting2.setOnClickListener(this);
+        btnFaq.setOnClickListener(this);
+        //btnFb.setOnClickListener(this);
         btnAbout.setOnClickListener(this);
         btnLogout.setOnClickListener(this);
         return rootView;
@@ -82,20 +91,24 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @SuppressWarnings("UnusedParameters")
     private void initInstances(View rootView, Bundle savedInstanceState) {
         // Init 'View' instance(s) with rootView.findViewById here
+        btnEdit = (Button) rootView.findViewById(R.id.profile_edit_profile_btn);
+
         btnFavourite = (LinearLayout) rootView.findViewById(R.id.profile_favourite_btn);
         btnReserved = (LinearLayout) rootView.findViewById(R.id.profile_reserved_btn);
-        btnEdit = (LinearLayout) rootView.findViewById(R.id.profile_edit_profile_btn);
-        btnSetting = (Button) rootView.findViewById(R.id.profile_setting_btn);
-        btnFb = (Button) rootView.findViewById(R.id.profile_fb_btn);
-        btnAbout = (Button) rootView.findViewById(R.id.profile_about_btn);
+        btnSetting = (LinearLayout) rootView.findViewById(R.id.profile_setting_btn);
+        btnSetting2 = (LinearLayout) rootView.findViewById(R.id.profile_setting2_btn);
+        btnFaq = (LinearLayout) rootView.findViewById(R.id.profile_faq_btn);
+        btnAbout = (LinearLayout) rootView.findViewById(R.id.profile_about_btn);
+
+        //btnFb = (Button) rootView.findViewById(R.id.profile_fb_btn);
         btnLogout = (Button) rootView.findViewById(R.id.profile_logout_btn);
         tvName = (TextView) rootView.findViewById(R.id.profile_name);
         tvSurname= (TextView) rootView.findViewById(R.id.profile_surname);
-         tvType= (TextView) rootView.findViewById(R.id.profile_type);
-        tvYear= (TextView) rootView.findViewById(R.id.profile_year);
-        tvSchool= (TextView) rootView.findViewById(R.id.profile_school);
-        tvAge= (TextView) rootView.findViewById(R.id.profile_age);
-        tvGender= (TextView) rootView.findViewById(R.id.profile_gender);
+        tvEmail = (TextView) rootView.findViewById(R.id.profile_email);
+        tvYear = (TextView) rootView.findViewById(R.id.profile_year);
+        tvSchool = (TextView) rootView.findViewById(R.id.profile_school);
+        tvAge = (TextView) rootView.findViewById(R.id.profile_age);
+        tvGender = (TextView) rootView.findViewById(R.id.profile_gender);
     }
 
     @Override
@@ -133,13 +146,41 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         } else if (v == btnReserved) {
             Intent intent = new Intent(getActivity(), ReservedActivity.class);
             getContext().startActivity(intent);
-        } else if( v== btnEdit){
+        } else if (v == btnEdit) {
 
-        }else if( v== btnSetting){
+        } else if (v == btnSetting) {
 
-        }else if( v== btnFb){
+        } else if (v == btnSetting2) {
+
+        }/*else if( v== btnFb){
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/1053263171395979"));
+                startActivity(intent);
+            } catch (Exception e) {
+                Intent intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/1053263171395979"));
+                startActivity(intent);
+            }
+
+        }*/else if( v== btnFaq){
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.main_overlay, FaqFragment.newInstance());
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
 
         }else if( v== btnAbout){
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.main_overlay, AboutFragment.newInstance());
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            /*final Dialog dialog = new Dialog(getContext());
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.fragment_about);
+            dialog.setCancelable(true);
+            dialog.show();*/
 
         }else if( v== btnLogout){
 
@@ -154,8 +195,16 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         tvSurname.setText(text);
     }
 
-    public void setType(String text){
-        tvType.setText(text);
+    public void setEmail(String text){
+        tvEmail.setText(text);
+    }
+
+    public void setAge(String text){
+        tvAge.setText(text);
+    }
+
+    public void setGender(String text){
+        tvGender.setText(text);
     }
 
     public void setYear(String text){
@@ -166,11 +215,5 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         tvSchool.setText(text);
     }
 
-    public void setAge(String text){
-        tvAge.setText(text);
-    }
 
-    public void setGender(String text){
-        tvGender.setText(text);
-    }
 }
