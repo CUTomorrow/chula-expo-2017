699300421eac492b0d5cf35b97388d0a2d222274
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-03-06 04:07:52 +0700

    new activity

---
+++ ChulaExpo/app/src/main/java/cuexpo/chulaexpo/activity/DoneRegisterActivity.java
@@ -0,0 +1,15 @@
+package cuexpo.chulaexpo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cuexpo.chulaexpo.R;
+
+public class DoneRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done_register);
+    }
+}
