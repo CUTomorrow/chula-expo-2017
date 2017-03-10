b8f2ac125a37525f84c8a581e21b72cda4177ee2
James Rakratchatakul <james.rak@outlook.co.th>
2017-03-10 16:29:10 +0700

    OnClick Highlight

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/adapter/HighlightListAdapter.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/adapter/HighlightListAdapter.java
@@ -1,11 +1,22 @@
 package cuexpo.cuexpo2017.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import cuexpo.cuexpo2017.R;
 import cuexpo.cuexpo2017.dao.ActivityItemCollectionDao;
 import cuexpo.cuexpo2017.dao.ActivityItemResultDao;
+import cuexpo.cuexpo2017.fragment.EventDetailFragment;
 import cuexpo.cuexpo2017.view.HighlightListItem;
 
 /**
@@ -15,7 +26,10 @@ import cuexpo.cuexpo2017.view.HighlightListItem;
 public class HighlightListAdapter extends PagerAdapter {
 
     ActivityItemCollectionDao dao;
-
+    Fragment fragment;
+    public HighlightListAdapter(Fragment fragment) {
+        this.fragment = fragment;
+    }
     public void setDao(ActivityItemCollectionDao dao) {
         this.dao = dao;
     }
@@ -27,20 +41,38 @@ public class HighlightListAdapter extends PagerAdapter {
         return dao.getResults().size();
     }
 
-
     @Override
     public boolean isViewFromObject(View view, Object object) {
         return view == object;
     }
 
     @Override
-    public Object instantiateItem(ViewGroup container, int position) {
+    public Object instantiateItem(final ViewGroup container,final int position) {
         HighlightListItem item;
         item = new HighlightListItem(container.getContext());
-        ActivityItemResultDao result = dao.getResults().get(position);
+        final ActivityItemResultDao result = dao.getResults().get(position);
         item.setNameText(result.getName().getTh());
         item.setDescriptionText(result.getShortDescription().getTh());
         item.setImageUrl("https://staff.chulaexpo.com" + result.getBanner());
+
+        //LayoutInflater inflater = (LayoutInflater) container.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ImageView ivHighlight = item.getIvHighlight();
+        //ImageView ivHighlight = (ImageView) page.findViewById(R.id.ivHighlight);
+        ivHighlight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences activitySharedPref = container.getContext().getSharedPreferences("Event", Context.MODE_PRIVATE);
+                String activityId = result.getId();
+                Log.e("highlight","Activity ID: "+activityId);
+                activitySharedPref.edit().putString("EventID", activityId).apply();
+
+                FragmentManager fragmentManager = fragment.getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.container, new EventDetailFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
         /*mock highlight
         if(position%3==0){
             item.setNameText("ละครนิทรรศ 17");
