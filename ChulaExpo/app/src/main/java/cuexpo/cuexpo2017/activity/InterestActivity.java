39198325bfbc2fa126ef5d9a4bc410a70ae7b8f7
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-03-11 15:50:44 +0700

    start interest activity

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/activity/InterestActivity.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/activity/InterestActivity.java
@@ -1,44 +1,50 @@
 package cuexpo.cuexpo2017.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import cuexpo.cuexpo2017.R;
 import cuexpo.cuexpo2017.datatype.InterestItem;
 
 
 public class InterestActivity extends AppCompatActivity {
     private Activity activity;
-    ArrayList<InterestItem> interestItems;
+    ArrayList<InterestItem> interestItems = new ArrayList<>();
     TextView selectedText;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_interest);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interest);
+
+        activity = this;
+        setInterestItems();
 //
-//        activity = this;
-//        interestItems = new ArrayList<>();
-//        setInterestItems();
-//
-//        LayoutInflater inflater = getLayoutInflater();
-//        View gridViewFooter = inflater.inflate(R.layout.item_interest_footer, null);
+        LayoutInflater inflater = getLayoutInflater();
+        View gridViewFooter = inflater.inflate(R.layout.item_interest_footer, null);
 //
 //        GridViewWithHeaderAndFooter gridView = (GridViewWithHeaderAndFooter) findViewById(R.id.grid_view);
 //        gridView.addFooterView(gridViewFooter);
 //        gridView.setAdapter(new InterestListAdapter(this, interestItems));
 //        gridView.setOnItemClickListener(onItemClick);
 //
-//        ImageView doneBtn = (ImageView) findViewById(R.id.done_btn);
-//        doneBtn.setOnClickListener(doneListener);
+        ImageView doneBtn = (ImageView) findViewById(R.id.done_btn);
+        doneBtn.setOnClickListener(doneListener);
 //
 //        selectedText = (TextView) findViewById(R.id.selected);
 //        setSelectedText();
-//    }
+    }
 //
-//    private void setInterestItems(){
+    private void setInterestItems(){
 //        String[] images = { "http://keenthemes.com/preview/metronic/theme/assets/global/plugins/jcrop/demos/demo_files/image1.jpg"
 //        };
 //        String[] titles = {
@@ -52,7 +58,7 @@ public class InterestActivity extends AppCompatActivity {
 //            InterestItem interestItem = new InterestItem(titles[i], images[0], isInterested[i]);
 //            interestItems.add(interestItem);
 //        }
-//    }
+    }
 //
 //    public int dpToPx(int dp) {
 //        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
@@ -60,31 +66,31 @@ public class InterestActivity extends AppCompatActivity {
 //    }
 //
 //
-//    private View.OnClickListener doneListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            Intent intent = new Intent(activity, MainActivity.class);
-//            startActivity(intent);
-//        }
-//    };
-//
-//    private AdapterView.OnItemClickListener onItemClick = new AdapterView.OnItemClickListener(){
-//        @Override
-//        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-//            InterestItem interestItem = interestItems.get(position);
-//            ImageView checkImage = (ImageView) v.findViewById(R.id.check_image);
-//            if(interestItem.isInterest()) {
-//                checkImage.setVisibility(View.INVISIBLE);
-//                interestItem.setInterest(false);
-//            }
-//            else {
-//                checkImage.setVisibility(View.VISIBLE);
-//                interestItem.setInterest(true);
-//            }
-//            setSelectedText();
-//        }
-//    };
-//
+    private View.OnClickListener doneListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(activity, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private AdapterView.OnItemClickListener onItemClick = new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            InterestItem interestItem = interestItems.get(position);
+            ImageView checkImage = (ImageView) v.findViewById(R.id.check_image);
+            if(interestItem.isInterest()) {
+                checkImage.setVisibility(View.INVISIBLE);
+                interestItem.setInterest(false);
+            }
+            else {
+                checkImage.setVisibility(View.VISIBLE);
+                interestItem.setInterest(true);
+            }
+            setSelectedText();
+        }
+    };
+
     private void setSelectedText() {
         int selectedItem = 0;
         int totalItem = 0;
