412fb262aef706dbdc6e4a9c5df0a28455dd2bc8
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-03-12 02:25:50 +0700

    compact code

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/utility/FacultyMapEntity.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/utility/FacultyMapEntity.java
@@ -88,82 +88,45 @@ public class FacultyMapEntity implements IMapEntity {
 //        return Resource.getDrawable("pin_"+facultyId);
         switch (facultyId) {
             // Area
-            case 1:
-                return R.drawable.pin_1;
-            case 2:
-                return R.drawable.pin_2;
-            case 3:
-                return R.drawable.pin_3;
-            case 4:
-                return R.drawable.pin_4;
-            case 5:
-                return R.drawable.shop;
-            case 6:
-                return R.drawable.sponsor;
-            case 7:
-                return R.drawable.sponsor;
+            case 1: return R.drawable.pin_1;
+            case 2: return R.drawable.pin_2;
+            case 3: return R.drawable.pin_3;
+            case 4: return R.drawable.pin_4;
+            case 5: return R.drawable.shop;
+            case 6: return R.drawable.sponsor;
+            case 7: return R.drawable.sponsor;
             // City
-            case 10:
-                return R.drawable.pin_10;
-            case 11:
-                return R.drawable.pin_11;
-            case 12:
-                return R.drawable.pin_12;
-            case 13:
-                return R.drawable.pin_12;
+            case 10: return R.drawable.pin_10;
+            case 11: return R.drawable.pin_11;
+            case 12: return R.drawable.pin_12;
+            case 13: return R.drawable.pin_12;
             // Faculty
-            case 21:
-                return R.drawable.pin_21;
-            case 22:
-                return R.drawable.pin_22;
-            case 23:
-                return R.drawable.pin_23;
-            case 24:
-                return R.drawable.pin_24;
-            case 25:
-                return R.drawable.pin_25;
-            case 26:
-                return R.drawable.pin_26;
-            case 27:
-                return R.drawable.pin_27;
-            case 28:
-                return R.drawable.pin_28;
-            case 29:
-                return R.drawable.pin_29;
-            case 30:
-                return R.drawable.pin_30;
-            case 31:
-                return R.drawable.pin_31;
-            case 20:
-                return R.drawable.pin_31;
-            case 32:
-                return R.drawable.pin_32;
-            case 33:
-                return R.drawable.pin_33;
-            case 34:
-                return R.drawable.pin_34;
-            case 35:
-                return R.drawable.pin_35;
-            case 36:
-                return R.drawable.pin_36;
-            case 37:
-                return R.drawable.pin_37;
-            case 38:
-                return R.drawable.pin_38;
-            case 39:
-                return R.drawable.pin_39;
-            case 40:
-                return R.drawable.pin_40;
-            case 41:
-                return R.drawable.pin_41;
-            case 42:
-                return R.drawable.pin_42;
-            case 43:
-                return R.drawable.pin_43;
-            case 44:
-                return R.drawable.pin_43;
-            case 45:
-                return R.drawable.pin_43;
+            case 21: return R.drawable.pin_21;
+            case 22: return R.drawable.pin_22;
+            case 23: return R.drawable.pin_23;
+            case 24: return R.drawable.pin_24;
+            case 25: return R.drawable.pin_25;
+            case 26: return R.drawable.pin_26;
+            case 27: return R.drawable.pin_27;
+            case 28: return R.drawable.pin_28;
+            case 29: return R.drawable.pin_29;
+            case 30: return R.drawable.pin_30;
+            case 31: return R.drawable.pin_31;
+            case 20: return R.drawable.pin_31;
+            case 32: return R.drawable.pin_32;
+            case 33: return R.drawable.pin_33;
+            case 34: return R.drawable.pin_34;
+            case 35: return R.drawable.pin_35;
+            case 36: return R.drawable.pin_36;
+            case 37: return R.drawable.pin_37;
+            case 38: return R.drawable.pin_38;
+            case 39: return R.drawable.pin_39;
+            case 40: return R.drawable.pin_40;
+            case 41: return R.drawable.pin_41;
+            case 42: return R.drawable.pin_42;
+            case 43: return R.drawable.pin_43;
+            case 44: return R.drawable.pin_43;
+            case 45: return R.drawable.pin_43;
 
             default:
                 throw new IllegalArgumentException("Invalid faculty id : " + facultyId);
