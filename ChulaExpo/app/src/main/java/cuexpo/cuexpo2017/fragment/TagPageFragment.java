f5d063952a74cec07999e353716e545d6d88428a
Teerapat Prompunjai <theerapat_nick@yahoo.com>
2017-03-12 23:34:19 +0700

    add resource from PNG to JPG

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/TagPageFragment.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/TagPageFragment.java
@@ -7,14 +7,21 @@ import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ListView;
 
+import java.util.List;
+
 import cuexpo.cuexpo2017.R;
+import cuexpo.cuexpo2017.adapter.InterestListAdapterNew;
+import cuexpo.cuexpo2017.adapter.TagPageAdapter;
+import cuexpo.cuexpo2017.dao.ActivityItemResultDao;
 
 public class TagPageFragment extends Fragment {
 
     private ListView list;
-
+    private TagPageAdapter adapter;
+    private List<ActivityItemResultDao> eventList;
 
     public TagPageFragment() {
         // Required empty public constructor
@@ -25,10 +32,21 @@ public class TagPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_tag_page, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_tag_page, container, false);
+        initInstances(rootView, savedInstanceState);
+        return rootView;
     }
 
 
+    @SuppressWarnings("UnusedParameters")
+    private void initInstances(View rootView, Bundle savedInstanceState) {
+        // Init 'View' instance(s) with rootView.findViewById here
+
+        list = (ListView) rootView.findViewById(R.id.list_view);
+        adapter = new TagPageAdapter(getContext());
+
+
+
+    }
 
 }
