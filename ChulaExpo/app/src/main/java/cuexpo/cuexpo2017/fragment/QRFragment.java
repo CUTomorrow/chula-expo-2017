f8894b17eccfaace15225aa3ae043ab9d8cd01ec
James Rakratchatakul <james.rak@outlook.co.th>
2017-03-13 00:25:10 +0700

    QR Redesign

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/QRFragment.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/QRFragment.java
@@ -5,12 +5,18 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +24,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import net.glxn.qrgen.android.QRCode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import cuexpo.cuexpo2017.R;
 import cuexpo.cuexpo2017.activity.MainActivity;
@@ -150,8 +164,17 @@ public class QRFragment extends Fragment implements View.OnClickListener {
         else tvQRPersonalInfo.setText("");
 
         try {
-            Bitmap qrBm = QRCode.from((String) tvQRName.getText()).bitmap();
-            ivQR.setImageBitmap(qrBm);
+            //Bitmap qrBm = QRCode.from((String) tvQRName.getText()).bitmap();
+            //ivQR.setImageBitmap(qrBm);
+
+            String charset = "UTF-8";
+            Map<EncodeHintType, ErrorCorrectionLevel> hintMap =new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            String qrCodeData = tvQRName.getText().toString();
+            int width =300;
+            int height = 300;
+            int smallestDimension = width < height ? width : height;
+            CreateQRCode(qrCodeData, charset, hintMap, smallestDimension, smallestDimension);
         } catch (Exception e){
 
         }
@@ -162,5 +185,61 @@ public class QRFragment extends Fragment implements View.OnClickListener {
         if(v==ivClear){
             getFragmentManager().popBackStack();
         }
+    }
+
+    public  void CreateQRCode(String qrCodeData, String charset, Map hintMap, int qrCodeheight, int qrCodewidth){
+
+
+        try {
+            //generating qr code in bitmatrix type
+            BitMatrix matrix = new MultiFormatWriter().encode(new String(qrCodeData.getBytes(charset), charset),
+                    BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
+            //converting bitmatrix to bitmap
+
+            int width = matrix.getWidth();
+            int height = matrix.getHeight();
+            int[] pixels = new int[width * height];
+            // All are 0, or black, by default
+            for (int y = 0; y < height; y++) {
+                int offset = y * width;
+                for (int x = 0; x < width; x++) {
+                    //pixels[offset + x] = matrix.get(x, y) ? BLACK : WHITE;
+                    pixels[offset + x] = matrix.get(x, y) ?
+                            ResourcesCompat.getColor(getResources(),R.color.black,null) :  ResourcesCompat.getColor(getResources(),R.color.white,null);
+                }
+            }
+
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+            //setting bitmap to image view
+
+            Bitmap overlay = BitmapFactory.decodeResource(getResources(), R.drawable.iv_qr_icon);
+
+            ivQR.setImageBitmap(mergeBitmaps(overlay,bitmap));
+
+        }catch (Exception er){
+            Log.e("QrGenerate",er.getMessage());
+        }
+    }
+
+
+
+    public Bitmap mergeBitmaps(Bitmap overlay, Bitmap bitmap) {
+
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+
+        Bitmap combined = Bitmap.createBitmap(width, height, bitmap.getConfig());
+        Canvas canvas = new Canvas(combined);
+        int canvasWidth = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+
+        canvas.drawBitmap(bitmap, new Matrix(), null);
+
+        int centreX = (canvasWidth  - overlay.getWidth()) /2;
+        int centreY = (canvasHeight - overlay.getHeight()) /2 ;
+        canvas.drawBitmap(overlay, centreX, centreY, null);
+
+        return combined;
     }
 }
