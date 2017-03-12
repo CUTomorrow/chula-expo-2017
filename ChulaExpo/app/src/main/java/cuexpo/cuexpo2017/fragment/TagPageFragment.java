b262b41b885ce2588e7c64fe852c312ac415d85e
Teerapat Prompunjai <theerapat_nick@yahoo.com>
2017-03-12 20:53:17 +0700

    fix 4 last faculty

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/TagPageFragment.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/TagPageFragment.java
@@ -3,17 +3,17 @@ package cuexpo.cuexpo2017.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import cuexpo.cuexpo2017.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class TagPageFragment extends Fragment {
 
+    private ListView list;
 
 
     public TagPageFragment() {
@@ -25,6 +25,7 @@ public class TagPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_tag_page, container, false);
     }
 
