0f5b35d6d74771f8252c9e23dde040528a27ea9f
James Rakratchatakul <james.rak@outlook.co.th>
2017-03-13 23:49:53 +0700

    get UserInfo in MainAct

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/activity/MainActivity.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -96,39 +97,51 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         sharedPref = getSharedPreferences("FacebookInfo", MODE_PRIVATE);
         if(!sharedPref.getString("apiToken","").equals("")){
             HttpManager.getInstance().setAPIKey(sharedPref.getString("apiToken",""));
-            /*
+
             Call<UserDao> callUser = HttpManager.getInstance().getService().loadUserInfo();
             callUser.enqueue(new Callback<UserDao>() {
                 @Override
                 public void onResponse(Call<UserDao> call, Response<UserDao> response) {
-                    Log.e("UserInfo","Load UserInfo Success");
-                    if(response.isSuccessful()) {
+                    Log.e("UserInfo", "Load UserInfo Success");
+                    if (response.isSuccessful()) {
                         UserDao dao = response.body();
-                        if(dao.getSuccess()){
-                            Log.e("UserInfo","Success=true");
+                        if (dao.getSuccess()) {
+                            Log.e("UserInfo", "Success=true");
                             //keep token
                             SharedPreferences sharedPref = getSharedPreferences("FacebookInfo", MODE_PRIVATE);
                             SharedPreferences.Editor editor = sharedPref.edit();
-                            editor.putString("id",dao.getResults().getId());
-                            editor.putString("type",dao.getResults().getId());
-                            editor.putString("profile",dao.getResults().getId());
-                            editor.putString("age",dao.getResults().getId());
-                            editor.putString("gender",dao.getResults().getId());
-                            
-
-                            //Log.e("LoginFB","apiToken = " + dao.getResults().getToken());
+                            editor.putString("id", dao.getResults().getId());
+                            editor.putString("type", dao.getResults().getType());
+                            editor.putString("profile", dao.getResults().getProfile());
+                            editor.putString("gender", dao.getResults().getGender());
+                            editor.putString("name",dao.getResults().getName());
+                            editor.putString("emails",dao.getResults().getEmail());
+                            try {
+                                editor.putInt("age", dao.getResults().getAge());
+                            } catch (Exception e){
+                                editor.putInt("age", 0);
+                            }
+                            if(dao.getResults().getType().equals("Academic")) {
+                                editor.putString("academicSchool", dao.getResults().getAcademic().getAcademicSchool());
+                                editor.putString("academicYear", dao.getResults().getAcademic().getAcademicYear());
+                                editor.putString("academicLevel", dao.getResults().getAcademic().getAcademicLevel());
+                            } else if(dao.getResults().getType().equals("Worker")){
+                                editor.putString("workerJob",dao.getResults().getWorker().getJob());
+                            }
                             editor.apply();
-                }
+                        } else {
 
+                        }
+                    } else {
+
+                    }
+                }
                 @Override
                 public void onFailure(Call<UserDao> call, Throwable t) {
 
                 }
             });
-            */
         }
-
-
     }
 
     @Override
