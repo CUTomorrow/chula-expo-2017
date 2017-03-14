0da4ea07bc66ff28863364855ef20753fedaf0ad
Kosate Limpongsa <kosatelim@gmail.com>
2017-03-15 00:58:39 +0700

    T_T Forget to push

---
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/dao/LoginUser.java
@@ -0,0 +1,15 @@
+package cuexpo.cuexpo2017.dao;
+
+/**
+ * Created by neungkl on 3/14/2017 AD.
+ */
+
+public class LoginUser {
+    private String email;
+    private String password;
+
+    public LoginUser(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
