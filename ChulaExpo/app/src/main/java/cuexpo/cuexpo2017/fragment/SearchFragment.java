5ab53ad22b775b2e248450cba708323e84b2362a
5730593821-SP <supakritboom@hotmail.com>
2017-03-15 12:40:02 +0700

    Remove Special API

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/SearchFragment.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/fragment/SearchFragment.java
@@ -2,36 +2,25 @@ package cuexpo.cuexpo2017.fragment;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import cuexpo.cuexpo2017.MainApplication;
@@ -39,11 +28,7 @@ import cuexpo.cuexpo2017.R;
 import cuexpo.cuexpo2017.adapter.SearchListAdapter;
 import cuexpo.cuexpo2017.dao.ActivityItemCollectionDao;
 import cuexpo.cuexpo2017.dao.ActivityItemResultDao;
-import cuexpo.cuexpo2017.dao.ActivitySearchItemCollectionDao;
-import cuexpo.cuexpo2017.dao.DeleteResultDao;
-import cuexpo.cuexpo2017.dao.NearbyDao;
 import cuexpo.cuexpo2017.manager.HttpManager;
-import cuexpo.cuexpo2017.manager.HttpManagerSpecial;
 import cuexpo.cuexpo2017.utility.DateUtil;
 import cuexpo.cuexpo2017.view.EventListItem;
 import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
@@ -122,14 +107,9 @@ public class SearchFragment extends Fragment {
         }
         Call<ActivityItemCollectionDao> callSearchActivities;
         Log.e("Search Fragment", "String : " + s);
-            /*if (checkMap) {
-                callSearchActivities = HttpManagerSpecial.getInstance().
-                        getService().searchActivities(id, lat, lng, 300, s);
-            } else {*/
         callSearchActivities =
                 HttpManager.getInstance().
                         getService().searchActivities(s);
-        //}
         callSearchActivities.enqueue(callbackSearch);
     }
 
