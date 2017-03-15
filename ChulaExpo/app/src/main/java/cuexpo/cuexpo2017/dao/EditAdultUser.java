092d4d7c76f49b6c138d2c8eed0e4f15027bcaba
James Rakratchatakul <james.rak@outlook.co.th>
2017-03-16 02:16:53 +0700

    Fix EditUserInfo

--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/dao/EditAdultUser.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/dao/EditAdultUser.java
@@ -8,12 +8,14 @@ public class EditAdultUser {
     private String name;
     private int age;
     private String gender;
+    private String type;
     private String workerJob;
 
-    public EditAdultUser(String name, int age, String gender, String workerJob) {
+    public EditAdultUser(String name, int age, String gender, String type, String workerJob) {
         this.name = name;
         this.age = age;
         this.gender = gender;
+        this.type = type;
         this.workerJob = workerJob;
     }
 }
--- ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/dao/EditStudentUser.java
+++ ChulaExpo/app/src/main/java/cuexpo/cuexpo2017/dao/EditStudentUser.java
@@ -8,14 +8,16 @@ public class EditStudentUser {
     private String name;
     private int age;
     private String gender;
+    private  String type;
     private String academicLevel;
     private String academicYear;
     private String academicSchool;
 
-    public EditStudentUser(String name, int age, String gender, String academicLevel, String academicYear, String academicSchool) {
+    public EditStudentUser(String name, int age, String gender, String type, String academicLevel, String academicYear, String academicSchool) {
         this.name = name;
         this.age = age;
         this.gender = gender;
+        this.type = type;
         this.academicLevel = academicLevel;
         this.academicYear = academicYear;
         this.academicSchool = academicSchool;
